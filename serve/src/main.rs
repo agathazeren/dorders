07966ff49a36468947a023e718887c9f2d4f1f1d
Asa Zeren <asaizeren@gmail.com>
2020-03-18 12:49:41 -0400

    This should be the correct directory this time

--- serve/src/main.rs
+++ serve/src/main.rs
@@ -65,7 +65,7 @@ fn main() {
     rocket::custom(
         ConfigBuilder::new(ENVIRONMENT)
             .port(std::env::vars().find(|(var,_)|var=="PORT").unwrap_or(("".to_string(),"8000".to_string())).1.parse::<u16>().unwrap())
-            .extra("template_dir","../../serve/templates")
+            .extra("template_dir","serve/templates")
             .log_level(LoggingLevel::Debug)
             .finalize().unwrap())
         .mount("/",routes![
